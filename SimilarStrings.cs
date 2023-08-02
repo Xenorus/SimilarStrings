ad246904ce333f77e4ff2c1b16623674e0e615e9
Alex <Alex@DESKTOP-05BJRQ3>
2023-08-02 16:03:27 +0500

    Add project files.

---
+++ SimilarStrings.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interview
+{
+    /* Задача:
+
+    Две различные строки называются похожими, если одну можно получить из другой удалением одного символа или 
+    заменой местами двух символов (не обязательно соседних).
+
+    Например, строка "korova" похожа на строки "kovora", "krova" и "korovan" и не похожа на строки "karova", "orov" и "okroav".
+
+    Даны две различные непустые строки, состоящие из строчных латинских букв. Нужно определить, являются ли они похожими.
+
+    Пример использования:
+    var result = new SimilarStringsChecker().CheckStrings("korova", "vorona");
+
+    Для проверки решения необходимо запустить тесты.
+   */
+
+    public class SimilarStringsChecker
+    {
+        public bool CheckStrings(string first, string second)
+        {
+            if (first.Length == second.Length)
+                return CheckSwapTwoCharacters(first, second);
+            if (first.Length == second.Length + 1)
+                return CheckRemoveCharacter(first, second);
+            if (first.Length == second.Length - 1)
+                return CheckRemoveCharacter(second, first);
+            return false;
+        }
+
+        private bool CheckSwapTwoCharacters(string firstString, string secondString)
+        {
+            char[,] dataAboutCharacters = new char[3, 3];
+            int swapCounter = 0;
+
+            for (int i = 0; i<firstString.Length; i++)
+            {
+                //если символы в строке не равны
+                if (firstString[i] != secondString[i])
+                {
+                    swapCounter++;
+                    if (swapCounter > 2)
+                    {
+                        //если перестановок больше 2
+                        return false;
+                    }
+
+                    if (swapCounter == 0)
+                    {
+                        dataAboutCharacters[0, 0] = firstString[i];
+                        dataAboutCharacters[0, 1] = secondString[i];
+                    }
+                    else {
+                        dataAboutCharacters[1, 0] = firstString[i];
+                        dataAboutCharacters[1, 1] = secondString[i];
+                    }
+                }
+            }
+
+            if ((dataAboutCharacters[0, 0] == dataAboutCharacters[1,0]) && (dataAboutCharacters[0, 1] == dataAboutCharacters[1, 1]))
+            {
+                return true;
+            } else { return false; }
+            
+            
+            throw new NotImplementedException();
+        }
+
+        private bool CheckRemoveCharacter(string longString, string shortString)
+        {
+            int matchCounter = 0;
+
+            char[] longStringChar = longString.ToCharArray();
+            char[] shortStringChar = shortString.ToCharArray();
+
+            for (int i = 0; i<longString.Length; i++)
+            {
+                if (i>shortString.Length)
+                {
+                    continue;
+                }
+
+                for (int j = 0; j<shortString.Length; j++)
+                {
+                    if ((longStringChar[i] == shortStringChar[j]) && (longStringChar[i] != '0') && (shortStringChar[j] != '0'))
+                    {
+                        longStringChar[i] = '0';
+                        shortStringChar[j] = '0';
+                        matchCounter++;
+                    }
+                }
+            }
+
+            if (matchCounter==shortString.Length)
+            {
+                return true;
+            } else { return false; }
+
+
+
+
+
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tests.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Interview
+{
+    // Если тесты не запускаются через Test Explorer, выполните Clean Solution
+    public class Tests
+    {
+        public static IEnumerable<object[]> TestCases()
+        {
+            yield return new object[] {"korova", "kovora", true};
+            yield return new object[] {"abc", "cab", false};
+            yield return new object[] {"korovv", "kovorr", false};
+            yield return new object[] {"korova", "krova", true};
+            yield return new object[] {"korova", "kroav", false};
+            yield return new object[] {"krova", "korova", true};
+            yield return new object[] {"korova", "korovan", true};
+            yield return new object[] {"korovan", "korova", true};
+            yield return new object[] {"korova", "vorona", false};
+            yield return new object[] {"korova", "rotova", false};
+            yield return new object[] {"rotova", "korova", false};
+            yield return new object[] {"korova", "karova", false};
+            yield return new object[] {"korova", "rokota", false};
+            yield return new object[] {"kororo", "korova", false};
+            yield return new object[] {"korova", "kororo", false};
+            yield return new object[] {"korova", "a", false};
+            yield return new object[] {"rama", "lama", false};
+            yield return new object[] {"kxova", "korova", false};
+            yield return new object[] {"abc", "abca", true};
+            yield return new object[] {"abcab", "abcb", true};
+            yield return new object[] {"abcab", "abca", true};
+            yield return new object[] {"abcab", "bcab", true};
+            yield return new object[] {"abcab", "acab", true};
+            yield return new object[] {"abcab", "abcba", true};
+            yield return new object[] {"abcbac", "abcabc", true};
+            yield return new object[] {"abracadabra", "acracadabra", false};
+            yield return new object[] {"aaaaaaab", "aaaaaaac", false};
+            yield return new object[] {"a", "b", false};
+            yield return new object[] {"a", "ab", true};
+            yield return new object[] {"ab", "a", true};
+            yield return new object[] {"ab", "ba", true};
+            yield return new object[] {"abcdab", "abcd", false};
+            yield return new object[] {"abcd", "abcdab", false};
+            yield return new object[] {"abcd", "bcx", false};
+            yield return new object[] {"bcd", "abcd", true};
+            yield return new object[] {"acd", "abcd", true};
+            yield return new object[] {"abd", "abcd", true};
+            yield return new object[] {"abc", "abcd", true};
+            yield return new object[] {"dbcd", "dbecd", true};
+            yield return new object[] {"dbecd", "dbcd", true};
+            yield return new object[] {"qwerty", "verty", false};
+            yield return new object[] {"verty", "qwerty", false};
+            yield return new object[] {"dbcd", "dbecx", false};
+            yield return new object[] {"dbecx", "dbcd", false};
+            yield return new object[] {"abcdefghijklmnopqrstuvwxyz", "zbcdefghijklmnopqrstuvwxya", true};
+            yield return new object[] {"abcdefghijklmnopqrstuvwxyz", "zbcdefthijklmnopqrsguvwxya", false};
+            yield return new object[] {"abcdefghijklmnopqrstuvwxyz", "bcdefghijklmnopqrstuvwxyza", false};
+            yield return new object[] {"abcdefghijklmnopqrstuvwxyz", "abcdefghijlmnopqrstuvwxyz", true};
+            yield return new object[] {"abcdefghijlmnopqrstuvwxyz", "abcdefghijklmnopqrstuvwxyz", true};
+            yield return new object[] {"aaaaaaa", "aaaa", false};
+            yield return new object[] {"aaaaaaa", "aaaaaa", true};
+            yield return new object[] {"aaaaab", "aaabb", false};
+            yield return new object[] {"bbbaa", "bbbbba", false};
+            yield return new object[] {"ababab", "bbaba", false};
+            yield return new object[] {"bb", "abc", false};
+            yield return new object[] {"abaaa", "babbb", false};
+            yield return new object[] {"aagaeaa", "aacaiaa", false};
+        }
+
+     [TestCaseSource(nameof(TestCases))]
+        public void Test(string first, string second, bool expected)
+        {
+            var actual = new SimilarStringsChecker().CheckStrings(first, second);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
